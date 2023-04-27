354496d49311304d64bf57389b813cae7fb63a78
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-04-27 15:11:03 -0500

    done

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+    @WebServlet(name = "CounterServlet", urlPatterns = "/count")
+    public class CounterServlet extends HttpServlet {
+        private int count = 0;
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.setContentType("text/html");
+            String reset = request.getParameter("reset");
+            PrintWriter out = response.getWriter();
+            if (reset == null) {
+                count++;
+                //out.println("<html>" + count + "</html>");
+            } else {
+                out.println("<html>" + "COUNT HAS BEEN RESET!!!" + "<html>");
+                count = 0;
+//                out.println("<html>" + count + "<html>");
+                //reset = 0;
+            }
+            out.println("<html>" + count + "<html>");
+        }
+    }
+
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+    public class HelloWorldServlet extends HttpServlet {
 
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+            response.setContentType("text/html");
+            String name = request.getParameter("name");
+            PrintWriter out = response.getWriter();
+
+            if (name == null) {
+                out.println("Why hello there...");
+            } else {
+                out.println("Why hello there..." + name);
+            }
+        }
     }
-}
