00330b31806d007398bb604667b1296ed608042b
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-05-04 09:23:07 -0500

    almost done

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,9 @@
+public class BeanTest {
+    public static void main(String[] args) {
+        AlbumBean myAlbum = new AlbumBean();
+        AuthorBean myAuthor = new AuthorBean();
+        QuoteBean myQuote = new QuoteBean();
+
+        //still need #4
+    }
+}
